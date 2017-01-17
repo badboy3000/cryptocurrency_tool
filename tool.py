3db8f4ff3aa8e3c478c324c7c7d01283c15c3eef
grumpydevelop <simsim0331@gmail.com>
2017-01-17 15:37:56 +0300

    Add files via upload

--- tool.py
+++ tool.py
@@ -1,20 +1,31 @@
+"""
+Donations:
+BTC: 1Px3sRDEcdC6mmpx4FofyeYnUFvcpmdYhp
+DASH: XraofDfAeHxgZZ7khTR3bcAzf3EXiveXuw
+DOGE: DBZ1Qbh4P18UESVeA8oB5YFaimojR1Vq9E
+LTC: LSNZkKq59oSHSHQmZMS4SncfMXoakePrKc
+ETH: 0xabf2443445edc00e1c32a5b5f3c8479f51b9b4cb (min. 1eth)
+"""
+
 import fileinput
 import zipfile
 import requests
 import filefind
 import sys
-try:
+
+try:  # Lol, great protection system
     f = open('.age.txt')
     if int(f.read()) < int(18):
-        print("Fuck, we dont need any shitcoins")
+        print("Just close program, kid")
         sys.exit()
 except FileNotFoundError:
     old = int(input("How old are you? \n"))
     if old < 18:
-        print("Fuck, we dont need any shitcoins")
+        print("Just close program, kid")
         file = open(".age.txt", "w")
         file.write(str(old))
         file.close()
+        sys.exit()
     else:
         print("First run configure.sh file")
         print("Don't forget to chmod u+x configure.sh")
@@ -37,7 +48,7 @@ except FileNotFoundError:
                 textToReplace = input("Name of your coin? Like Litecoin or Bitcoin (Uppercase) ")
                 textToReplace1 = input("Name of your coin in lowercase ")
                 textToReplace3 = input("Name of your coin with CAPS LOCK ")
-                texts1 = input("Name of your coin like TLC, BTC ")
+                texts1 = input("Name of your coin like LTC, BTC ")
                 fileToSearch = "pos/blackcoin-master/README.md"
                 fileToSearch = ','.join(filefind.get_filepaths(directory="pos"))
                 tempFile = open(fileToSearch, 'r+')
@@ -58,9 +69,9 @@ except FileNotFoundError:
                 url = "https://github.com/litecoin-project/litecoin/archive/v0.13.2.1.zip"
                 r = requests.get(url)
                 with open("pow.zip", "wb") as code:
-                    code.write(r.content) # Downloading sources
+                    code.write(r.content)  # Downloading sources
                 with zipfile.ZipFile("pow.zip", "r") as zip_ref:
-                    zip_ref.extractall("pow") # Unzip sources into 'pow' folder
+                    zip_ref.extractall("pow")  # Unzip sources into 'pow' folder
                 textToSearch = "Litecoin"
                 textToSearch1 = "litecoin"
                 textToSearch2 = "LITECOIN"
@@ -69,7 +80,7 @@ except FileNotFoundError:
                 textToReplace1 = input("Name of your coin in lowercase ")
                 textToReplace3 = input("Name of your coin with CAPS LOCK ")
                 texts1 = input("Name of your coin like TLC, BTC ")
-                fileToSearch = ' , '.join(filefind.get_filepaths(directory="pow"))
+                fileToSearch = str(''.join(filefind.get_filepaths(directory="pow")))  # TODO: Fix paths
                 tempFile = open(fileToSearch, 'r+')
                 for line in fileinput.input(fileToSearch):
                     tempFile.write(line.replace(textToSearch, textToReplace))
@@ -96,4 +107,4 @@ except FileNotFoundError:
                     tempFile.write(line.replace(textToSearch1, ports1))
                 tempFile.close()
         else:
-            print|("Bye.")
+            print("Bye.")
